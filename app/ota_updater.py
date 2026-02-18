c8523a755ba5bc93ff33efadbb31b05b6aaa090b
mihaitruta2 <38535070+mihaitruta2@users.noreply.github.com>
2026-02-18 23:31:55 +0000

    Update ota_updater.py

--- app/ota_updater.py
+++ app/ota_updater.py
@@ -141,7 +141,7 @@ class OTAUpdater:
         print('Version {} downloaded to {}'.format(version, self.modulepath(self.new_version_dir)))
 
     def _download_all_files(self, version, sub_dir=''):
-        url = 'https://api.github.com/repos/{}/contents{}{}{}?ref=refs/tags/{}'.format(self.github_repo, self.github_src_dir, self.main_dir, sub_dir, version)
+        url = 'https://api.github.com/repos/{}/contents/{}{}{}?ref=refs/tags/{}'.format(self.github_repo, self.github_src_dir, self.main_dir, sub_dir, version)
         gc.collect() 
         file_list = self.http_client.get(url)
         file_list_json = file_list.json()
